39e5005a4ce49d2eb4b7ba0f375cd5d23b1de3ce
Zoey <zokuro@live.fr>
2021-03-06 14:12:09 +0000

    Updates

---
+++ component---src-pages-main-js-9173db65e6ccdc715629.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_minimal=self.webpackChunkgatsby_starter_minimal||[]).push([[450],{988:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return u}});var a=t(294),r=function(){return a.createElement("nav",{class:"h-10 bg-pink-500 w-auto"})},u=function(){return a.createElement("main",null,a.createElement(r,null))}}}]);
+//# sourceMappingURL=component---src-pages-main-js-9173db65e6ccdc715629.js.map
