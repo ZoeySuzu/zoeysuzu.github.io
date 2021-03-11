be54a9096e047a0f6f47bf9f0afc5cbdcde02414
Zoey <zokuro@live.fr>
2021-03-11 22:31:05 +0000

    Navbar and Footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+
+const Footer = () => {
+    return(
+        <nav class="bg-gradient-to-tr from-purple-600 to-purple-800 border-t-2 border-purple-300 border-solid flex bot">
+            <div class="flex-col self-center mx-auto">
+                <h3 class="text-lg text-gray-100 ">Copyright ©2021 Zoey Suzumiya</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Footer
+
---
+++ src/components/navbar/menuItems.js
@@ -0,0 +1,34 @@
+const MenuItems = [
+    {
+        title:'Home',
+        url:'#',
+        cName:'',
+        enabled: true
+    },
+    {
+        title:'About Me',
+        url:'#',
+        cName:'',
+        enabled: true
+    },
+    {
+        title:'Projects',
+        url:'#',
+        cName:'',
+        enabled: true
+    },
+    {
+        title:'Games',
+        url:'#',
+        cName:'',
+        enabled: true
+    },
+    {
+        title:'Blog',
+        url:'#',
+        cName:'',
+        enabled: true
+    },
+]
+
+export default MenuItems
